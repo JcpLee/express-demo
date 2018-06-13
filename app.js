238bef18c4c6f775ee23af6cc6e0a19575dd6585
JcpLee <875056558@qq.com>
2018-06-13 16:57:29 +0800

    first commit

--- app.js
+++ app.js
@@ -10,9 +10,34 @@ app.get('/token', function (req, res) {
   res.send("token");
 })
 
-app.get('*', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+// app.get('*', function (req, res) {
+//   res.render('index', { title: 'Hey', message: 'Hello!' })
+// })
+
+app.get('/login', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello!' })
 })
+
+app.get('/list', function(req, res) {
+  res.render('list', {
+
+
+    list: [
+    {id: 1, name: "zzz",gender: 'M'},
+    {id: 2, name: "xxx",gender: 'F'},
+    {id: 3, name: "xxx",gender: 'F'},
+    {id: 2, name: "xxx",gender: 'F'},
+    {id: 2, name: "xxx",gender: 'F'},
+    {id: 2, name: "xxx",gender: 'F'},
+  ]})
+})
+
+app.get('/test',function(req,res,next){
+  res.render('index.html');
+
+});
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
